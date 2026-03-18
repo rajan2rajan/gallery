1e5a5321ac6fde70b84788ece84547311709f69e
Rajan Bhandari@rajan <rajanbhandari939@gmail.com>
2026-03-18 12:37:00 +1100

    Update API URL to new backend

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api'; // Make sure this matches your backend port
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api'; // Make sure this matches your backend port
 
 
 const api = axios.create({
